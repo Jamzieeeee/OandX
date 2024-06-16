e87bb15b11c01c2d99388602d677d2f39e1ec30f
James Whitaker <james@altrion.org>
2024-06-16 20:07:40 +0000

    Add function to set opponent difficulty

--- run.py
+++ run.py
@@ -12,11 +12,42 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('OandX')
 
+""" GET USERNAME """
+
 def enter_username():
     """
     Get username from the user
     """
     username = input("Please enter a username:")
-    print(f"Welcome {username}")
+    print(f"Welcome {username}\n")
 
 enter_username()
+
+""" SELECT DIFFICULTY """
+
+def select_difficulty():
+    """
+    Ask and set game difficulty
+    """
+    print("Please set your opponent difficulty\n")
+    print("Easy: Chooses moves at random")
+    print("Hard: Blocks lines of 2, but still random")
+    print("Perfect: Plays perfectly and CANNOT be beaten\n")
+    dif_str = input("Enter difficulty as spelled above: ")
+    if dif_str == "Easy":
+        difficulty = 1
+        print("You have selected 'Easy' difficulty!")
+    elif dif_str == "Hard":
+        difficulty = 2
+        print("You have selected 'Hard' difficulty!")
+    elif dif_str == "Perfect":
+        difficulty = 3
+        print("You have selected 'Perfect' difficulty!")
+    else:
+        raise ValueError(
+            f"{dif_str} is not a valid difficulty!"
+        )
+        dif_str = input("Please enter 'Easy', 'Hard', or 'Perfect': ")
+
+
+select_difficulty()
