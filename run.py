49fa92b20818a72958501d546490a817381c3329
James Whitaker <james@altrion.org>
2024-06-20 20:33:07 +0000

    Add reset method to fix box already marked bug

--- run.py
+++ run.py
@@ -95,6 +95,7 @@ def select_difficulty():
             print("You have selected 'Easy' difficulty!")
             sleep(1)
             clear()
+            reset()
             print_board(initial=True)
             play(first_player, turn)
             break
@@ -120,10 +121,22 @@ winning_combos = [  [0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6],
                     [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6], ]
 
 
+def reset():
+    """
+    Resets game board and turn number
+    """
+    global boxes
+    global turn
+    boxes = [ ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ]
+    turn = 1
+
+
+
 def print_board(initial=False):
-    """ Print the game board. If this is the beginning of the game,
-        print out 1-9 in the boxes to show players how to pick a
-        box. Otherwise, update each box with X or 0 from boxes[].
+    """ 
+    Print the game board. If this is the beginning of the game,
+    print out 1-9 in the boxes to show players how to pick a
+    box. Otherwise, update each box with X or 0 from boxes[].
     """
     print('''
              {} | {} | {} 
@@ -135,8 +148,9 @@ def print_board(initial=False):
 
 
 def take_turn(player, turn):
-    """ Create a loop that keeps asking the current player for
-        their input until a valid choice is made.
+    """ 
+    Create a loop that keeps asking the current player for
+    their input until a valid choice is made.
     """
 
     while True:
@@ -164,25 +178,28 @@ def take_turn(player, turn):
 
 
 def get_computer_move():
-    """ Return a random integer from 0 to 8, inclusive
+    """ 
+    Return a random integer from 0 to 8, inclusive
     """
     return random.randint(0,8)
         
 
 def switch_player(turn):
-    """ Switch the player based on how many moves have been made.
-        X starts the game so if this turn # is even, it's 0's turn. 
+    """ 
+    Switch the player based on how many moves have been made.
+    X starts the game so if this turn # is even, it's 0's turn. 
     """
     current_player = COMPUTER if turn % 2 == 0 else HUMAN
     return current_player
 
 
 def check_for_win(player, turn):
-    """ Check for a win (or a tie). For each combo in winning_combos[],
-        count how many of its corresponding squares have the current 
-        player's mark. If a player's score count reaches 3, return a win.
-        If it doesn't, and this is already turn # 9, return a tie. If
-        neither, return False so the game continues.
+    """ 
+    Check for a win (or a tie). For each combo in winning_combos[],
+    count how many of its corresponding squares have the current 
+    player's mark. If a player's score count reaches 3, return a win.
+    If it doesn't, and this is already turn # 9, return a tie. If
+    neither, return False so the game continues.
     """
     if turn > 4: # need at least 5 moves before a win is possible
         for combo in winning_combos:
@@ -198,8 +215,9 @@ def check_for_win(player, turn):
 
 
 def play(player, turn):
-    """ Create a loop that keeps the game in play
-        until it ends in a win or tie
+    """ 
+    Create a loop that keeps the game in play
+    until it ends in a win or tie
     """
     while True:
         take_turn(player, turn)
@@ -218,6 +236,9 @@ def play(player, turn):
 
 
 def game_over():
+    """
+    Show options after game ends
+    """
     sleep(1)
     print("1 = Play again")
     print("2 = Change difficulty")
@@ -229,6 +250,7 @@ def game_over():
         
         if game_over_str == "1":
             clear()
+            reset()
             print_board(initial=True)
             play(first_player, turn)
             break
